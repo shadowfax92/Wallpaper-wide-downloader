68c00820604ecb8fd9ce49d6a5c07b4f948bd548
nikhil sonti <nikhilsv92@gmail.com>
2015-02-06 14:55:14 +0530

    [main.go] extra and fetch resolution written. will change the struct to match hdwallpapers.net

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+/*
+References:
+- HTML parsing using goquery example - https://www.socketloop.com/tutorials/golang-how-to-extract-links-from-web-page
+- goquery documentation - http://godoc.org/github.com/PuerkitoBio/goquery#Selection
+*/
+
 import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
@@ -8,13 +14,33 @@ import (
 )
 
 const (
-	base_url = "http://wallpaperswide.com/"
+	base_url            = "http://wallpaperswide.com/"
+	wallpaper_page_base = "http://wallpaperswide.com/mac-desktop-wallpapers/page/"
+	download_resolution = "1920x1080"
 )
 
-func main() {
-	doc, err := goquery.NewDocument("http://wallpaperswide.com/mac-desktop-wallpapers.html")
+func fetchDownloadableUrl(url string) (err error) {
+	log.Println("\nGetting needed resolution image from url = ", url)
+	doc, err := goquery.NewDocument(url)
 	if err != nil {
-		log.Fatalf("Error querying url", err)
+		log.Fatalln("Error while fetching downloadable url form url = ", url)
+	}
+	curr_selction := doc.Find(".wallpaper-resolutions a")
+	curr_selction.Each(func(i int, s *goquery.Selection) {
+		resolution := s.Text()
+		if resolution == download_resolution {
+			href, _ := s.Attr("href")
+			fmt.Println("Found resolution = ", resolution, " href = ", href)
+		}
+	})
+	return err
+}
+
+func extractUrls(url string) (err error) {
+	log.Println("\nExtracting from url = ", url)
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		log.Fatalln("Error parsing url = ", url)
 	}
 
 	/*
@@ -31,7 +57,25 @@ func main() {
 	// Try to get extract the above sub-html
 	curr_sel := doc.Find(".wallpapers .wall .thumb .mini-hud a")
 	curr_sel.Each(func(i int, s *goquery.Selection) {
-		hrefs, _ := s.Attr("href")
-		fmt.Println(hrefs)
+		hrefs, is_present := s.Attr("href")
+		if is_present == true {
+			log.Println("Image url is ", base_url+hrefs)
+			fetchDownloadableUrl(base_url + hrefs)
+		}
 	})
+	return err
+}
+
+func main() {
+	fmt.Println("Wallpaper downlaoder by Nsonti")
+	var page_start int
+	var page_end int
+	fmt.Println("Enter page_start and page_end")
+	_, _ = fmt.Scanf("%d", &page_start)
+	_, _ = fmt.Scanf("%d", &page_end)
+
+	for i := page_start; i <= page_end; i++ {
+		extractUrls(wallpaper_page_base + string(i))
+	}
+
 }
